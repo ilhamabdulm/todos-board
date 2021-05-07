2477172a60d7e8732a6e9ae4b1bb20dcc241d46b
Ilham <a.marzmellow21@gmail.com>
2021-05-07 13:03:42 +0700

    update validation for min value and dropdown content position on last item

--- src/components/fragments/dropdown/index.jsx
+++ src/components/fragments/dropdown/index.jsx
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
 const Dropdown = (props) => {
-  const { children, menu } = props;
+  const { children, menu, position } = props;
 
   return (
     <div className={styles.dropdown}>
       {children}
-      <div className={styles['dropdown-content']}>
+      <div
+        className={styles['dropdown-content']}
+        style={{
+          right: position === 'left' && 0,
+          left: position === 'right' && 0,
+        }}
+      >
         {menu.map((mn) => (
           <p onClick={mn.action}>{mn.text}</p>
         ))}
@@ -20,11 +26,13 @@ const Dropdown = (props) => {
 Dropdown.defaultProps = {
   children: null,
   menu: [],
+  position: 'left',
 };
 
 Dropdown.propTypes = {
   children: PropTypes.node,
   menu: PropTypes.array,
+  position: 'left' | 'right',
 };
 
 export default Dropdown;
